ddc2e7a3b2b3bf2d87dfa20bf084b29d8b49ff1e
Jose Sierra <Jose.Sierra0@walmart.com>
2022-04-19 09:43:00 -0600

    Section 10 DONE!

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
 import { app } from './app';
 
+// configuration only
 const start = async () => {
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
