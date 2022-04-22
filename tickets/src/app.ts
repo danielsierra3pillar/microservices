e5bf093d6d4fb3bfb426dfb22592a0e69923e0e0
Jose Sierra <Jose.Sierra0@walmart.com>
2022-04-21 23:08:48 -0600

    More changes in section 12, added the tickets service with more logic

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -2,8 +2,10 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
-import { errorHandler } from '@danielsierra3pillartickets/common';
+import { currentUser, errorHandler } from '@danielsierra3pillartickets/common';
 import { NotFoundError } from '@danielsierra3pillartickets/common';
+import { createTicketRouter } from './routes/new';
+import { showTicketRouter } from './routes/show';
 
 const app = express();
 app.set('trust proxy', true);
@@ -14,6 +16,11 @@ app.use(
     secure: process.env.NODE_ENV !== 'test',
   })
 );
+
+app.use(currentUser);
+
+app.use(createTicketRouter);
+app.use(showTicketRouter);
 
 app.all('*', async (req, res) => {
   throw new NotFoundError();
