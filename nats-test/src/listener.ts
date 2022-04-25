ebcceda93bd4a9ed5d663358b46f9e137e77de4e
Jose Sierra <Jose.Sierra0@walmart.com>
2022-04-25 09:56:29 -0600

    Test of nasts condigured manually

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -10,9 +10,13 @@ const client = nats.connect('ticketing', randomBytes(4).toString('hex'), {
 client.on('connect', () => {
   console.log('Listener connected to NATS');
 
+  // Notify NATS manually that a msessage has been recieved
+  const options = client.subscriptionOptions().setManualAckMode(true);
+
   const subscription = client.subscribe(
     'ticket:created',
-    'orders-service-queue-group'
+    'orders-service-queue-group',
+    options
   );
   // @ts-ignore
   subscription.on('message', (message: Message) => {
@@ -24,5 +28,9 @@ client.on('connect', () => {
       console.log(`Received event #${message.getSequence()}`);
       console.log(`Received data #${data}`);
     }
+
+    // This is to tell node streaming library to reach back out the NATS
+    // and tell hey! the message we recieved it has been processesd
+    message.ack();
   });
 });
