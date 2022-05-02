aa8985947555f3f6be878ad8bf2a2a252ff1e46c
Jose Sierra <Jose.Sierra0@walmart.com>
2022-05-02 11:23:40 -0600

    How to get and set in node js world

--- tickets/src/nats-wrapper.ts
+++ tickets/src/nats-wrapper.ts
@@ -3,17 +3,25 @@ import nats, { Stan } from 'node-nats-streaming';
 class NatsWrapper {
   private _client?: Stan;
 
+  get client() {
+    if (!this._client) {
+      throw new Error('Cannot acces NATS client before connecting');
+    }
+
+    return this._client;
+  }
+
   // create an instance of nats and then assgin it to _client
   connect(cluster_id: string, client_id: string, url: string) {
     this._client = nats.connect(cluster_id, client_id, { url });
 
     return new Promise<void>((resolve, reject) => {
-      this._client?.on('connect', () => {
+      this.client.on('connect', () => {
         console.log('Connected to NATS!');
         resolve();
       });
 
-      this._client?.on('connect', (error) => {
+      this.client.on('connect', (error) => {
         console.log('Connected to NATS!');
         reject(error);
       });
